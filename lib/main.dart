b59ceae42c56723f4328335af2808ce497335e5e
Aakash <aakkidesai4567@gmail.com>
2025-07-30 14:48:02 +0530

    Satyam mali Mc Added

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Aakash Desai Maderchod'),
+      home: const MyHomePage(title: 'Akash Patil Maderchod'),
     );
   }
 }
